5603ff87838dd63aadd8cd1edbc2658625527b04
Max Tayler <maxtayler1@gmail.com>
2024-02-08 11:44:03 +0000

    Fix Compose previews in design-system module.

--- shared/design-system/build.gradle.kts
+++ shared/design-system/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling.main)
     implementation(libs.androidx.compose.activity)
     implementation(libs.androidx.compose.navigation)
 }
